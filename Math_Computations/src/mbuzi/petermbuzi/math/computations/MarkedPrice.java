9b0d9fc8a6875657d6894c0a7434c9ce5a6d653d
petermbuzi <petermbuzi84@gmail.com>
2020-12-21 00:05:11 +0300

    marked price java file

---
+++ Math_Computations/src/mbuzi/petermbuzi/math/computations/MarkedPrice.java
@@ -0,0 +1,17 @@
+package mbuzi.petermbuzi.math.computations;
+
+public class MarkedPrice {
+
+    public double markedPriceDiscountSellingPrice(double discount, double selling_price) {
+        return discount + selling_price;
+    }
+
+    public double markedPriceDiscountPerDiscount(double discount, double percentage_discount) {
+        return (100 * discount) / percentage_discount;
+    }
+
+    public double markedPriceSellingPricePerDiscount(double selling_price, double percentage_discount) {
+        double den = 100 - percentage_discount;
+        return (100 * selling_price) / den;
+    }
+}
