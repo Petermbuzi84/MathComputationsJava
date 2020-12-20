bbc0cd6979840a67bd7a59dcb750a3007a92b370
petermbuzi <petermbuzi84@gmail.com>
2020-12-20 23:59:44 +0300

    addtion of loss file

---
+++ Math_Computations/src/mbuzi/petermbuzi/math/computations/Loss.java
@@ -0,0 +1,21 @@
+package mbuzi.petermbuzi.math.computations;
+
+public class Loss {
+
+    public double lossBuyingSellingPrice(double buying_price, double selling_price) {
+        return buying_price - selling_price;
+    }
+
+    public double lossPerLossBuyingPrice(double percentage_loss, double buying_price) {
+        return (percentage_loss * buying_price) / 100;
+    }
+
+    public double perLossLossBuyingPrice(double loss, double buying_price) {
+        return (100 * loss) / buying_price;
+    }
+
+    public double perLossBuyingSellingPrice(double buying_price, double selling_price) {
+        double loss = buying_price - selling_price;
+        return (100 * loss) / buying_price;
+    }
+}
